84079f2f611d481d0a6551982f0b2aa845ad5119
jmoana <jhenycruz7@gmail.com>
2021-03-31 22:17:30 -0300

    avaliação

--- application/views/avaliacoes/avaliacao.php
+++ application/views/avaliacoes/avaliacao.php
@@ -3,11 +3,11 @@
 
 <style>
 
-.estrelas{
-  margin-left:50em;
-}
 .estrelas input{
   display: none;
+}
+.estrelas label i.fa{
+  font-size: 2.5em;
 }
 .estrelas label i.fa:before {
   content:'\f005';
@@ -48,7 +48,7 @@
 .opcao-almoco,.opcao-lanche,.opcao-jantar{
   color: #6A6A6A!important;
   position:relative;
-  left:35%;
+  left:33%;
   text-align: center;
   text-decoration: none;
   display: inline-block;
@@ -58,10 +58,7 @@
   transition-duration: 0.4s;
   cursor: pointer;
 }
-.data-refeicao,.comentario{
-  margin-top:2em;
-margin-left:7em;
-}
+
 .dia-refei,.mes-refei,.ano-refei{
   height:2.3em; 
   border: 1.5px solid #CECECE;
@@ -72,6 +69,49 @@ width:4em;
 }
 .dia-refei,.mes-refei,.ano-refei{
 margin-left: 1em;
+}
+.estrelas,.estrelas2,.estrelas3 {
+  margin-left:50em;
+
+}
+.btn-cancelar {
+        border-color: #77C043;
+        border-radius: 5px;
+        color:#DE4545;  
+        border: 2px solid #f44336;
+        padding: 7px 45px;
+      }
+
+.btn-cancelar:hover {
+  background-color: #f44336;
+  color: white;
+  
+}
+
+.btn-salvar{
+        background-color: #4CAF50;
+        border-color: #77C043;
+        color: #FFFF!important;
+        border-radius: 5px;
+        border: none;
+        padding: 10px 55px;
+      }
+
+.btn-salvar:hover {
+  background-color: #198754;
+}
+
+.btn-salvar,.btn-cancelar{
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 10px 40px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+
+}
+i{
+  color: #DE4545;
 }
 </style>
 
@@ -86,7 +126,7 @@ margin-left: 1em;
              </form>
             </div>
  
-            <div class="data-refeicao" id="primeira-linha"> 
+            <div class="data-refeicao" style="margin:2em 0em 0em 8em;"> 
                 <label for="datrefei" class="form-label" >Data da resposta do cadastro  <i>*</i></label>                
                 <input class="dia-refei" data-val="true" data-val-length="Dia do Nascimento não pode exceder 2 caracteres" data-val-length-max="2" data-val-range="O dia deve estar entre 1 e 31." data-val-range-max="31" data-val-range-min="1" data-val-required="O campo Dia é obrigatório" type="text" value="" />
                 <input class="mes-refei" data-val="true" data-val-length="Mês do Nascimento não pode exceder 2 caracteres" data-val-length-max="2" data-val-range="O mês deve estar entre 1 e 12." data-val-range-max="12" data-val-range-min="1" data-val-required="O campo Mês é obrigatório" type="text" value="" />
@@ -95,7 +135,7 @@ margin-left: 1em;
             </div>
 
             
-              <div class="estrelas" id="primeira-linha">
+              <div class="estrelas" >
               <label class="comida" for="" style="">Alimento</label>
               <input type="radio" id="cm_star-empty" name="fb" value="" checked/>
               <label for="cm_star-1"><i class="fa"></i></label>
@@ -109,15 +149,23 @@ margin-left: 1em;
               <label for="cm_star-5"><i class="fa"></i></label>
               <input type="radio" id="cm_star-5" name="fb" value="5"/>
             </div>
+            
 
             <div class="col-sm-6">
-                <div class="comentario">
-                <label for="comen" class="form-label">Comentários <i>*</i></label>
+                <div class="comentario" style="margin-left:8em;">
+                <label for="comen" class="form-label">Comentários</label>
                 <input type="text" class="form-control" id="comen-tario" placeholder="" value="" required style="width:28.9em;height:4em;">
                 <div class="invalid-feedback">Inválido.</div>
                 </div> 
               </div>
 
-
+              <div class="botoes" style="text-align:center;margin:6em 0em 0em 29em;">   
+              <div class="botoes2">
+              <form class="d-flex">
+              <a  href="" class="btn-cancelar"><b>Cancelar</b></a> 
+              <a  href="" class="btn-salvar"><b>Salvar</b></a>
+             </form>
+              </div>
+            </div>
 
 </div>
