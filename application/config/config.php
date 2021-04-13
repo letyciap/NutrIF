2b7bd4367644a8bad575c9b0b1858bc73327e120
Letycia Pinheiro <56917549+letyciap@users.noreply.github.com>
2021-04-13 16:43:47 -0300

    Rota-Heroku

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/NutrIF';
+//$config['base_url'] = 'https://nutr-if.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
